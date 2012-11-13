9424e93115de976f7e76fcaca79e9754c60b9cef
farid.galeev <golfarid@gmail.com>
2012-11-13 08:30:28 +0400

    Create quattro project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quattro::Application.config.secret_token = 'b1eb5a18e4d53bd4089bd379f1b9fa8e3ec9c03f035e09d37f0a7c905bdd9d5b76fa76f1f971ebd903f6e87b0705501446db15671d7b693c18b18a3874671c02'
