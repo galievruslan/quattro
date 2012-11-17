c4b8a14079be86e9d7c9a104cb608cb63c6ed6e3
Galiev Ruslan <galievruslan@gmail.com>
2012-11-17 13:01:12 +0400

    fix brand model

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,7 +1,6 @@
 class Brand < ActiveRecord::Base
-  attr_accessible :model_id, :name
+  attr_accessible :name
   has_many :models
-  belong_to :vehicle
-  validates :model_id, :presence => true
+  belongs_to :vehicle
   validates :name, :presence => true, :length   => { :maximum => 250 }
 end
