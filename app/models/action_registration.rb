9ad49bf780288c471f2ab75285365dbbb8de009c
Farid Galeev <golfarid@gmail.com>
2012-11-24 16:13:52 +0400

    fix actio_registration model

--- app/models/action_registration.rb
+++ app/models/action_registration.rb
@@ -1,6 +1,7 @@
 class ActionRegistration < ActiveRecord::Base
   attr_accessible :customer_id, :vehicle_id
-  has_many :vehicles, :customers 
+  has_many :vehicles
+  has_many :customers
   validates :customer_id, :presence => true
   validates :vehicle_id, :presence => true
 end
