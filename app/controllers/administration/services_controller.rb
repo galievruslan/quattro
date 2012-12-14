5e80e39b4a6ab3173656d0a32f4513bb472f7a32
Ruslan Galiev <galievruslan@gmail.com>
2012-12-14 17:01:17 +0400

    add twitter bootstrap to administration page

--- app/controllers/administration/services_controller.rb
+++ app/controllers/administration/services_controller.rb
@@ -44,7 +44,7 @@ class Administration::ServicesController < AdministrationController
 
     respond_to do |format|
       if @service.save
-        format.html { redirect_to @service, notice: 'Service was successfully created.' }
+        format.html { redirect_to administration_services_url, notice: 'Service was successfully created.' }
         format.json { render json: @service, status: :created, location: @service }
       else
         format.html { render action: "new" }
