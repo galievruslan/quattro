e5f7962b0b9ddc58e1e97e5f1482ab843c533a30
Ruslan Galiev <galievruslan@gmail.com>
2012-12-28 15:45:37 +0400

    Add locale RU

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,14 @@
 module ApplicationHelper
   # Return a title on a per-page basis.
+  # def title
+  #   base_title = "Quattro team"
+  #   if @title.nil?
+  #     base_title
+  #   else
+  #     "#{base_title} | #{@title}"
+  #   end
+  # end
   def title
-    base_title = "Quattro team"
-    if @title.nil?
-      base_title
-    else
-      "#{base_title} | #{@title}"
-    end
+  	"Quattro - auto team"
   end
 end
